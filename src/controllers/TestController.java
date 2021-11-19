f4c77204fa1c51a0f8103506c2a72aafdac53b0f
Nguyen Duy Long <ngduylong442k@gmail.com>
2021-11-19 21:42:59 +0700

    Buoi 3

---
+++ src/controllers/TestController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+
+public class TestController {
+	@FXML
+	private AnchorPane anchorPane;
+	@FXML
+	private Button testBtn;
+	
+	@FXML
+	public void bonk() {
+		BackgroundImage myBI= new BackgroundImage(new Image("D:/workspace/eclipse-workspace/Project/CheemsBonk/src/assets/images/after_bonk.PNG"), null, null, null, null);
+		anchorPane.setBackground(new Background(myBI));
+	} //..\assets\images\book1.jpg
+	
+	@FXML
+	public void unBonk() {
+		BackgroundImage myBI= new BackgroundImage(new Image("D:/workspace/eclipse-workspace/Project/CheemsBonk/src/assets/images/before_bonk.PNG"), null, null, null, null);
+		anchorPane.setBackground(new Background(myBI));
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,7 @@
 module AIMS {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	opens controllers to javafx.graphics, javafx.fxml;
 }
